6f6774b55903e49f0305912dd4a225ef9e29c359
CHUANG <3418732870@qq.com>
2026-01-23 21:06:39 +0800

    将文件的中文名改为英文名

--- exercise.c
+++ exercise.c
@@ -4,15 +4,22 @@
 /* run this program using the console pauser or add your own getch,
  * system("pause") or input loop */
 
-int
-main(void)
+int main(void)
 {
-    int i         = 0;
-    int k         = 0;
-    int mat[3][3] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+    int i = 0;
+    int k = 0;
+    int mat[3][3] = {
 
-    for (i = 0; i < 3; i++) {
-        for (k = 0; k < 3; k++) {
+        {1, 2, 3},
+
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    for (i = 0; i < 3; i++)
+    {
+        for (k = 0; k < 3; k++)
+        {
             printf(" %p \n", mat[i] + k);
 
             //	printf("%p \n",mat[i]);
@@ -20,7 +27,8 @@ main(void)
         }
     }
     printf("\n \n");
-    for (i = 0; i < 3; i++) {
+    for (i = 0; i < 3; i++)
+    {
         printf(" %p \n", mat + 1);
     }
     printf(" \n \n");
