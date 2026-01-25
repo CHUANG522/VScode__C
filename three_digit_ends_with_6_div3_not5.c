d2af2dfa933f3f1af2f5d385a317c36ac7a11af8
CHUANG <3418732870@qq.com>
2026-01-25 21:56:02 +0800

    Update three_digit_ends_with_6_div3_not5.c

--- three_digit_ends_with_6_div3_not5.c
+++ three_digit_ends_with_6_div3_not5.c
@@ -1,43 +1,36 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+
 /* run this program using the console pauser or add your own getch() */
-int main() {
-/*  9.  个位数为6且能被3整除但不能被5整除的三位自然数共有多少个，分别是哪些
-	int x, i, I = 0;
-int count=0;
+int main()
+{
+    /*  9.  锟斤拷位锟斤拷为6锟斤拷锟杰憋拷3锟斤拷锟斤拷锟斤拷锟斤拷锟杰憋拷5锟斤拷锟斤拷锟斤拷锟斤拷位锟斤拷然锟斤拷锟斤拷锟叫讹拷锟劫革拷锟斤拷锟街憋拷锟斤拷锟斤拷些
+        int x, i, I = 0;
+    int count=0;
 
-	for (i = 1; i < 10; i++) {
-		for (I = 0; I < 10; I++) {
+        for (i = 1; i < 10; i++) {
+            for (I = 0; I < 10; I++) {
 
-			x = i * 100 + I * 10 + 6;
-			//	printf("%d\n",x);
-			if (x % 3 == 0  && x % 5 != 0) {
-				printf("%d\n", x);
-				count++;
-			}
+                x = i * 100 + I * 10 + 6;
+                //	printf("%d\n",x);
+                if (x % 3 == 0  && x % 5 != 0) {
+                    printf("%d\n", x);
+                    count++;
+                }
 
-		}
-
-
-
-
-	}printf("count is %d",count);*/
-
-struct average k{
-	int a=0;
-	double m=0;
-};
+            }
 
 
 
 
+        }printf("count is %d",count);*/
 
-	return 0;
+    struct average k
+    {
+        int a = 0;
+        double m = 0;
+    };
+
+    return 0;
 }
-
-
-
-
-
-
