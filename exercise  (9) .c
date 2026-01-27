ad18ea032a56a31d6d5f04a1c6c69924977f1a21
CHUANG <3418732870@qq.com>
2026-01-27 21:08:31 +0800

    Update exercise  (9) .c

--- exercise  (9) .c
+++ exercise  (9) .c
@@ -3,14 +3,12 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) {
-	
-	
-	
-	
-	
-	
-	
-	
-	return 0;
+int main(void)
+{
+    int i = 0;
+    char string[] = "world!";
+    printf("%c", string[i++]);
+    printf("%d", i);
+
+    return 0;
 }
