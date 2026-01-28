2489c0ea4f140a69a08545456900add6816e9066
CHUANG <3418732870@qq.com>
2026-01-28 22:36:11 +0800

    Update deleteSpaceInString.c

--- deleteSpaceInString.c
+++ deleteSpaceInString.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 // 题目：请编写一个函数，用来删除字符串中的所有空格。
-/* run this  program using the console pauser or add your own getch, system("pause") or input loop
+/* run this  program  using the console pauser or add your own getch, system("pause") or input loop
  */
 
 int main(void)
